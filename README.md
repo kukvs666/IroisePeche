5e44dd546fccb8daf7bc35094fb8012dbb1e1bc3
quentin GAUBERT <quentin.gaubert@wanadoo.fr>
2019-04-17 21:11:49 +0200

    site Iroise peche à valider par PO

---
+++ README.md
@@ -0,0 +1,5 @@
+# IroisePeche
+
+lien de prévisualisation du site Iroise Pêche:
+
+<a href="https://kukvs666.github.io/IroisePeche/" rel="nofollow">IroisePêche</a>
---
+++ script.js
@@ -0,0 +1,11 @@
+let quoiT = `<p>Iroise Pêche est un concept proposant à sa communauté des produits frais \
+de qualité, issus d une pêche responsable, respectant l environnement et les saisons.</p>\
+        <p>Découvrez chaque semaine notre newsletter, présentant le nouvel arrivage\
+            est pêché la veille par nos pêcheurs partenaires</p>\
+        <p>Il vous suffit de communiquer votre commande à Pascal qui vous livrera\
+            lui-même.</p>`
+let quiT =  `<p>L'histoire d'Iroise Pêche ,c'est avant celle d'un Breton passionné. Pascal\
+            KERBIRIOU, concepteur du projet, a eu l'envie il y a 4 ans de faire\
+            découvrir aux varrois les trésors de la mer d'Iroise. </p>`
+
+
